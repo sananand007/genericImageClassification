36fade339c7f14adb269e05215f989e08d6b8e85
sananand007 <anandjairam@ufl.edu>
2019-01-24 09:12:35 -0800

    adding tests

---
+++ tests.py
@@ -0,0 +1,13 @@
+'''
+Tests to Run on TRAVIS CI
+'''
+import sys, os
+import pandas as pd
+from glob import glob
+import numpy as np
+import time
+from imageCl import genImageCl
+#from tsp-local_2opt import Two_opt
+
+if __name__=="__main__":
+	print("Image Classification Tests")
